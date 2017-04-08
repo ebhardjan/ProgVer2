6fbce9dad4a4abc554a3b35c6f255d75fa407e9c
Jan Eberhardt <ebjan@student.ethz.ch>
2017-04-08 18:14:38 +0200

    adding some utility stuff for easier testing

--- VerifierProject/src/test/scala/core/TestUtils.scala
+++ VerifierProject/src/test/scala/core/TestUtils.scala
@@ -2,6 +2,10 @@ package core
 
 import java.io.File
 
+import viper.silver.frontend.SilFrontend
+import viper.silver.verifier.errors.AssertFailed
+import viper.silver.verifier.{Failure, Success, VerificationResult}
+
 /**
   * Created by jan on 07.04.17.
   *
@@ -26,6 +30,48 @@ object TestUtils {
     } else {
       List()
     }
+  }
+
+  def assertAssertionMightFail(verificationResult: VerificationResult, failedAssertionCount: Int): Unit = {
+    verificationResult match {
+      case Failure(errors) =>
+        val actualCount = errors.count(e => e.isInstanceOf[AssertFailed])
+        assert(actualCount == failedAssertionCount,
+          s"might-fail assertion count should be $failedAssertionCount but was $actualCount")
+      case Success =>
+        if (failedAssertionCount == 0) {
+          assert(true)
+        } else {
+          assert(assertion = false, s"no might-fail assertion found, there should be $failedAssertionCount")
+        }
+      case _ => throw new RuntimeException("Unexpected verification result")
+    }
+  }
+
+}
+
+/**
+  * similar to the Main object in MyVerifier.scala
+  */
+object TestingFrontend extends SilFrontend {
+
+  protected var verifierInstance: MyVerifier = _
+
+  def createVerifier(fullCmd: String): MyVerifier = {
+    verifierInstance = new MyVerifier()
+
+    verifierInstance
+  }
+
+  def configureVerifier(args: Seq[String]): Config = {
+    verifierInstance.parseCommandLine(args)
+    verifierInstance.start()
+
+    verifierInstance.config
+  }
+
+  def getResult: VerificationResult = {
+    _verificationResult.getOrElse(throw new RuntimeException("No verification result available"))
   }
 
 }
--- VerifierProject/src/test/scala/core/WlpStarTest.scala
+++ VerifierProject/src/test/scala/core/WlpStarTest.scala
@@ -8,11 +8,17 @@ import org.scalatest.FunSuite
 class WlpStarTest extends FunSuite {
 
   val dir = "src/test/resources/WlpStar/"
+  val fileRegex = ".*-amfc-(\\d+).vpr".r
 
   for (f <- TestUtils.getListOfVprFiles(dir)) {
     test("WlpStarTest-" + f) {
       val args: Array[String] = Array(dir + f)
-      Main.main(args)
+      TestingFrontend.execute(args)
+      val res = TestingFrontend.getResult
+
+      val mightFailCount: Int = (for (m <- fileRegex.findFirstMatchIn(f)) yield m.group(1)).get.toInt
+
+      TestUtils.assertAssertionMightFail(res, mightFailCount)
     }
   }
 
