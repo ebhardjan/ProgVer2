473fa6640da93b146d132e05978882988ad93cfe
Jan Eberhardt <ebjan@student.ethz.ch>
2017-04-20 15:11:41 +0200

    Added domains, axioms and triggers
    There are currently tests that fail. Possibly because the triggers are
    not handled correctly yet...

--- VerifierProject/src/main/scala/core/DeclarationCollector.scala
+++ VerifierProject/src/main/scala/core/DeclarationCollector.scala
@@ -1,24 +1,52 @@
 package core
 
+import smtlib.parser.Commands._
 import smtlib.parser.Terms._
-import viper.silver.ast._
+import util.ViperToSmtlibUtils
+import viper.silver.ast.{Bool, Domain, DomainFunc, DomainType, Int, LocalVarDecl, Program, Type}
 
 /**
   * Created by jan on 08.04.17.
   */
 object DeclarationCollector {
 
-
-  def collectDeclarations(locals: Seq[LocalVarDecl]): Map[String, Sort] = {
-    locals.map(l => l.name -> toSort(l.typ)).toMap
+  def collectDeclarations(program: Program, locals: Seq[LocalVarDecl]): Seq[Command] = {
+    collectDatatypeDeclarations(program.domains) ++
+      collectMethodLocalDeclarations(locals)
   }
 
-  def toSort(typ: Type): Sort = {
-    typ match {
-      case Int => Sort(SimpleIdentifier(SSymbol("Int")))
-      case Bool => Sort(SimpleIdentifier(SSymbol("Bool")))
-    }
+  private def collectDatatypeDeclarations(domains: Seq[Domain]): Seq[Command] = {
+    // TODO what exactly else do we need to support here? -> check what viper can do and the project descr.
+    var commands = Seq[Command]()
+    commands = commands :+
+      DeclareDatatypes(domains.map(d =>
+        (SSymbol(d.name), Seq(Constructor(SSymbol(d.name), Seq())))
+      ))
+    domains.foreach(d => commands = commands ++ collectFunctions(d.functions))
+    commands
+  }
 
+  private def collectFunctions(functions: Seq[DomainFunc]): Seq[Command] = {
+    functions.map(f =>
+      DeclareFun(
+        SSymbol(f.name),
+        f.formalArgs.map(a => ViperToSmtlibUtils.toSort(a.typ)),
+        ViperToSmtlibUtils.toSort(f.typ)
+      )
+    )
+  }
+
+  private def collectMethodLocalDeclarations(locals: Seq[LocalVarDecl]): Seq[Command] = {
+    locals.map(l => DeclareConst(SSymbol(l.name), ViperToSmtlibUtils.toSort(l.typ)))
+  }
+
+  def collectAxioms(domains: Seq[Domain]): Seq[Term] = {
+    // in case there are no axioms we just use (and true true)
+    var axioms = Seq[Term]()
+    domains.foreach(d => d.axioms.foreach(a => {
+      axioms = axioms :+ ViperToSmtlibUtils.toTerm(a.exp)
+    }))
+    axioms
   }
 
 }
--- VerifierProject/src/main/scala/core/WlpStar.scala
+++ VerifierProject/src/main/scala/core/WlpStar.scala
@@ -1,12 +1,10 @@
 package core
 
-import smtlib.parser.Terms
 import smtlib.parser.Terms._
+import smtlib.theories.Core
 import smtlib.theories.Core._
-import smtlib.theories.{Core, Ints}
-import smtlib.theories.Ints._
+import util.ViperToSmtlibUtils
 import viper.silver.ast._
-import viper.silver.ast
 
 /**
   * Created by jan on 07.04.17.
@@ -25,84 +23,13 @@ object WlpStar {
           wlpStar(ssFiltered.head, wlpStar(Seqn(ssFiltered.tail)(s.pos, s.info), delta))
         }
       case a@Assert(e) =>
-        delta + VerificationCondition(a, e, toTerm(e))
+        delta + VerificationCondition(a, e, ViperToSmtlibUtils.toTerm(e))
       case Inhale(exp) =>
-        val a1 = toTerm(exp)
+        val a1 = ViperToSmtlibUtils.toTerm(exp)
         delta.map(a => VerificationCondition(a.assert, a.exp, Core.Implies(a1, a.formula)))
       case NonDeterministicChoice(s1, s2) =>
         wlpStar(s1, delta) ++ wlpStar(s2, delta)
     }
-  }
-
-  /**
-    * converts a given silver expression into a smt-lib term
-    *
-    * note: the method is "formatted" in this ugly way (new lines after the case statements) so that we can collect
-    * collect line coverage information while running the tests and make sure that our test cover all possible
-    * expressions.
-    *
-    * @param exp a silver expression
-    * @return a smt-lib term
-    */
-  private def toTerm(exp: Exp): Term = {
-    exp match {
-      case ast.Add(left, right) =>
-        Ints.Add(toTerm(left), toTerm(right))
-      case ast.Sub(left, right) =>
-        Ints.Sub(toTerm(left), toTerm(right))
-      case ast.Mul(left, right) =>
-        Ints.Mul(toTerm(left), toTerm(right))
-      case ast.Div(left, right) =>
-        Ints.Div(toTerm(left), toTerm(right))
-      case ast.Mod(left, right) =>
-        Ints.Mod(toTerm(left), toTerm(right))
-
-      case Minus(e) =>
-        Neg(toTerm(e))
-
-      case LtCmp(left, right) =>
-        Ints.LessThan(toTerm(left), toTerm(right))
-      case LeCmp(left, right) =>
-        Ints.LessEquals(toTerm(left), toTerm(right))
-      case GtCmp(left, right) =>
-        Ints.GreaterThan(toTerm(left), toTerm(right))
-      case GeCmp(left, right) =>
-        Ints.GreaterEquals(toTerm(left), toTerm(right))
-
-      case EqCmp(left, right) =>
-        Equals(toTerm(left), toTerm(right))
-      case NeCmp(left, right) =>
-        Core.Not(Equals(toTerm(left), toTerm(right)))
-
-      case ast.Or(left, right) =>
-        Core.Or(toTerm(left), toTerm(right))
-      case ast.And(left, right) =>
-        Core.And(toTerm(left), toTerm(right))
-      case ast.Implies(left, right) =>
-        Core.Implies(toTerm(left), toTerm(right))
-      case ast.Not(e) =>
-        Core.Not(toTerm(e))
-
-      case TrueLit() =>
-        True()
-      case FalseLit() =>
-        False()
-
-      case LocalVar(name) =>
-        QualifiedIdentifier(SimpleIdentifier(SSymbol(name)))
-      case IntLit(i) =>
-        NumeralLit(i)
-
-      //TODO triggers!
-      case ast.Forall(vars, triggers, body) =>
-        Terms.Forall(sortedVar(vars.head), vars.tail.map(v => sortedVar(v)), toTerm(body))
-      case ast.Exists(vars, body) =>
-        Terms.Exists(sortedVar(vars.head), vars.tail.map(v => sortedVar(v)), toTerm(body))
-    }
-  }
-
-  def sortedVar(localVarDecl: LocalVarDecl): SortedVar = {
-    SortedVar(SSymbol(localVarDecl.name), DeclarationCollector.toSort(localVarDecl.typ))
   }
 
 }
